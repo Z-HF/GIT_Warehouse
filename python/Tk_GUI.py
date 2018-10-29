9e5eaddf24b36a13a6eef138c5aee159bb66e867
Z-HF <1159878350@qq.com>
2018-10-29 23:46:41 +0800

    buile a python GUI windows

---
+++ python/Tk_GUI.py
@@ -0,0 +1,18 @@
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+__author = {
+    "name": "Z-HF",
+    "e-mail": "1159878350@qq.com",
+    "date": "2018-10-29 23:44"
+}
+
+'''
+    make a gui windows
+'''
+
+import tkinter
+
+MainWindows = tkinter.Tk()
+
+MainWindows.mainloop()
